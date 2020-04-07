9272cedda901c54759d3afdba0613c9789270cf5
anik <anik.hasibulshamim@gmail.com>
2020-04-07 02:31:50 +0100

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+var darkMode = true;
+
+// function to check whether darkMode is active or not
+function check(variable){
+
+  if (variable == true) variable = false
+  else variable = true
+
+  return variable
+}
+
+
 // function to handle search bar animations
 $(document).ready(function () {
 
   var currentLocation = getLocation();
   console.log(currentLocation);
-
-  moveCursorToEnd(".search");
 
   $('.search').click(function () {
     $('.search').addClass('active');
@@ -41,29 +51,127 @@ $(document).ready(function () {
 $(document).keyup(function (event) {
 
   if ($(".search").is(":focus") && event.key == "Enter") {
+
+    $('.name').remove();
+
     var input = document.getElementById("search").value;
     var city = document.getElementById("img1");
     var block1 = document.getElementById("block1");
     var cityName = document.querySelector(".cityName");
+    var textRx = document.querySelector(".tR");
+    var textLx = document.querySelector(".tL");
+    var textLx2 = document.querySelector(".tL2");
 
-    if (input.includes(',')) {
-      var town = stripString(input);
-    } else {
-      town = input;
-    }
+    if (input.includes(',')) var town = stripString(input);
+    else town = input;
 
     var currentWeather = getWeather(town);
     console.log(currentWeather);
 
     // image icon
-    city.setAttribute("src", "icons/png/cityscape-l.png");
+    if (darkMode) city.setAttribute("src", "icons/png/cityscape-l.png");
+    else city.setAttribute("src", "icons/png/cityscape-d.png");
+
     city.setAttribute("width", "128");
     city.setAttribute("height", "128");
     city.setAttribute("title", "Town");
+
     // dashed line
-    block1.setAttribute("style", "border-bottom: 2.5px dashed #9ecfe1");
+    if (darkMode) block1.setAttribute("style", "border-bottom: 2.5px dashed #9ecfe1");
+    else block1.setAttribute("style", "border-bottom: 2.5px dashed #0a2239");
+
     // city name
     cityName.innerHTML = currentWeather.name + ", " + currentWeather.sys.country;
+    
+    // arrows
+    var arrowL = document.getElementById("aL");
+    var arrowR = document.getElementById("aR");
+
+    if (darkMode){
+      arrowL.setAttribute("src", "icons/png/arrow-l.png");
+      arrowR.setAttribute("src", "icons/png/arrow-l.png");
+    } 
+
+    else {
+      arrowL.setAttribute("src", "icons/png/arrow-d.png");
+      arrowR.setAttribute("src", "icons/png/arrow-d.png");
+    }
+
+    arrowL.setAttribute("width", "32");
+    arrowL.setAttribute("height", "32");
+    arrowR.setAttribute("width", "32");
+    arrowR.setAttribute("height", "32");
+
+    // temperature and description
+    var currentTemp = Math.round(currentWeather.main.temp);
+    textLx.innerHTML = currentTemp + "°C &nbsp;";
+
+    textRx.innerHTML = currentWeather.weather[0].description;
+    
+    var spaces = "&nbsp; &nbsp; &nbsp;"
+    textLx2.innerHTML = Math.round(currentWeather.main.temp_min) + "°C " +
+     spaces + "-" + spaces + Math.round(currentWeather.main.temp_max) + "°C" + "&nbsp;";
+    
+    // set temperature icon
+    if (currentTemp > 10 && currentTemp <= 20){
+
+      var tempIcon = document.getElementById("imgLeft");
+      
+      if (darkMode){
+         tempIcon.setAttribute("src", "icons/png/thermometer-warm-l.png");
+      }
+
+      else {
+        tempIcon.setAttribute("src", "icons/png/thermometer-warm-d.png");
+      }
+
+      tempIcon.setAttribute("width", "96");
+      tempIcon.setAttribute("height", "96");
+      tempIcon.setAttribute("title", "Temperature");
+    }
+
+    else if (currentTemp > 20) {
+
+      var tempIcon = document.getElementById("imgLeft");
+      
+      if (darkMode){
+         tempIcon.setAttribute("src", "icons/png/thermometer-hot-l.png");
+      }
+
+      else {
+        tempIcon.setAttribute("src", "icons/png/thermometer-hot-d.png");
+      }
+
+      tempIcon.setAttribute("width", "96");
+      tempIcon.setAttribute("height", "96");
+      tempIcon.setAttribute("title", "Temperature");
+    }
+
+    else if (currentTemp <= 10) {
+
+      var tempIcon = document.getElementById("imgLeft");
+      
+      if (darkMode){
+         tempIcon.setAttribute("src", "icons/png/thermometer-cold-l.png");
+      }
+
+      else {
+        tempIcon.setAttribute("src", "icons/png/thermometer-cold-d.png");
+      }
+
+      tempIcon.setAttribute("width", "96");
+      tempIcon.setAttribute("height", "96");
+      tempIcon.setAttribute("title", "Temperature");
+    } 
+
+    // set weather icon
+    var weatherIcon = document.getElementById("imgRight");
+
+    if (darkMode) weatherIcon.setAttribute("src", "icons/png/windy-l.png");
+    else weatherIcon.setAttribute("src", "icons/png/windy-d.png");
+    weatherIcon.setAttribute("width", "96");
+    weatherIcon.setAttribute("height", "96");
+    weatherIcon.setAttribute("title", "Current weather description");
   }
 })
 
@@ -73,7 +181,7 @@ function getLocation() {
 
   var result = null;
   var ipinfo_key = "2c85bba74c2c86"
-  var scriptUrl = "http://ipinfo.io?token=" + ipinfo_key;
+  var scriptUrl = "https://ipinfo.io?token=" + ipinfo_key;
   $.ajax({
     url: scriptUrl,
     type: 'get',
@@ -141,7 +249,6 @@ function getIcon(icon) {
   }
 }
 
-
 const icons = [
     { icon: "01d", equivalent: '25d.png' },
     { icon: "01n", equivalent: '14d.png' },
@@ -163,6 +270,7 @@ const icons = [
     { icon: "50n", equivalent: '18d.png' },
 ];
 
+
 // function to get the substring on the left hand-side of the (first) comma
 function stripString(string) {
 
@@ -173,28 +281,87 @@ function stripString(string) {
 }
 
 
-// function to enable dark mode, toggling and changing all necessary elements and styles
+// function to enable dark mode, toggling and changing the necessary styles (css)
 function enableDarkMode() {
 
   var body = document.body;
-  var element1 = document.getElementById("search");
-  var element2 = document.getElementById("line-1");
-  var element3 = document.getElementById("line-2");
+  var element0 = document.getElementById("search");
+  var element1 = document.getElementById("line-1");
+  var element2 = document.getElementById("line-2");
+  var element3 = document.getElementById("block4");
+
+  var element4 = document.getElementById("textLeft");
+  var element5 = document.getElementById("textLeft2");
+  var element6 = document.getElementById("textRight");
+
+  var element7 = document.getElementById("block1");
+  var el = document.getElementById("name");
 
   body.classList.toggle("dark-mode");
-  element1.classList.toggle("search-dark");
-  element2.classList.toggle("line-1-dark");
-  element3.classList.toggle("line-2-dark");
+  element0.classList.toggle("search-dark");
+  element1.classList.toggle("line-1-dark");
+  element2.classList.toggle("line-2-dark");
+  element3.classList.toggle("block4-dark");
+
+  element4.classList.toggle("textLeft-dark");
+  element5.classList.toggle("textLeft-dark");
+  element6.classList.toggle("textRight-dark");
+  element7.classList.toggle("block1-dark");
+  el.classList.toggle("name-dark");
+
+  darkMode = check(darkMode); // FIXME:
+  console.log("DarkMode: " + darkMode);
+}
+
+// function to enable dark mode, toggling and changing the necessary elements (icons)
+
+function myFunction() {
+
+  changeThermo();
+  changeScape();
+  changeName();
 }
 
 
-function moveCursorToEnd(el) {
-  if (typeof el.selectionStart == "number") {
-    el.selectionStart = el.selectionEnd = el.value.length;
-  } else if (typeof el.createTextRange != "undefined") {
-    el.focus();
-    var range = el.createTextRange();
-    range.collapse(false);
-    range.select();
+function changeThermo() {
+
+  var thermometerPath = "icons/png/thermometer-cold-";
+  var thermI = document.getElementById("imgLeft");
+
+  if (thermI.getAttribute("src") === thermometerPath+"d.png") {
+    thermI.setAttribute("src", thermometerPath+"l.png");
+  } else if (thermI.getAttribute("src") === thermometerPath+"l.png") {
+    thermI.setAttribute("src", thermometerPath+"d.png");
   }
+}
+
+function changeScape() {
+
+  var cityPath = "icons/png/cityscape-";
+  var city = document.getElementById("img1");
+
+  if (city.getAttribute("src") === cityPath+"l.png") {
+    city.setAttribute("src", cityPath+"d.png");
+  } else if (city.getAttribute("src") === cityPath+"d.png") {
+    city.setAttribute("src", cityPath+"l.png");
+  }
+}
+
+function changeName() {
+  
+  var el = document.getElementById("imgName");
+  if (el.getAttribute("src") === "pic1.png") {
+    el.setAttribute("src", "pic2.png");
+    el.setAttribute("height", "128");
+    el.setAttribute("width", "458");
+
+  }
+
+  else if (el.getAttribute("src") === "pic2.png") {
+    el.setAttribute("src", "pic1.png");
+    el.setAttribute("height", "128");
+    el.setAttribute("width", "458");
+
+  }
+
 }
